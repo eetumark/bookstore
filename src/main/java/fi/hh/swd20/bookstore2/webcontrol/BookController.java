1f56a8d1bf0bad31c0446417973186463f67f649
eetumark <eetu.mark@gmail.com>
2021-03-11 10:57:47 +0200

    lisätty REST ominaisuudet bookstoreen

--- src/main/java/fi/hh/swd20/bookstore2/webcontrol/BookController.java
+++ src/main/java/fi/hh/swd20/bookstore2/webcontrol/BookController.java
@@ -1,5 +1,9 @@
 package fi.hh.swd20.bookstore2.webcontrol;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicLong;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,6 +11,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import fi.hh.swd20.bookstore2.domain.Book;
 import fi.hh.swd20.bookstore2.domain.BookRepository;
@@ -16,6 +22,8 @@ import fi.hh.swd20.bookstore2.domain.CategoryRepository;
 
 @Controller
 public class BookController {
+	private final AtomicLong counter = new AtomicLong();
+	
 	
 	
 	@Autowired
@@ -28,6 +36,19 @@ public class BookController {
 				model.addAttribute("books", repository.findAll());
 				return "booklist";
 	}
+	
+	@RequestMapping(value= "/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> bookListRest(){
+				return (List<Book>) repository.findAll();
+	}
+	
+	@RequestMapping(value= "/books/{id}", method = RequestMethod.GET)
+	public @ResponseBody Optional<Book> findBookRest(@PathVariable("id") Long id){
+		return repository.findById(id);
+				
+	}
+	
+	
 	@RequestMapping(value= "/add")
 	public String addBook(Model model) {
 		model.addAttribute("book", new Book());
